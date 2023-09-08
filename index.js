8a675985295ad530ae9c4bb1bf97e11e6307e4a6
kmerten10 <kelsey.merten@gmail.com>
2023-09-08 11:11:23 -0700

    Exercise 2.5

--- index.js
+++ index.js
@@ -59,6 +59,42 @@ app.use((err, req, res, next)=>{
     res.status(500).send('Something Broke!');
 });
 
+app.get('/movies', (req, res) => {
+    res.send('Successful Get request returning list of movies')
+});
+
+app.get('/movies/[Title]', (req, res) => {
+    res.send('Successful Get request returning movie data by title')
+});
+
+app.get('/movies/[Title]/[Genre]', (req, res) => {
+    res.send('Successful Get request returning list of movies by genre')
+});
+
+app.get('/director', (req, res) => {
+    res.send('Successful Get request returning director information')
+});
+
+app.post('/username', (req, res) => {
+    res.send('Successful Post request creating new username')
+});
+
+app.put('/username', (req, res) => {
+    res.send('Successful Put request updated username')
+});
+
+app.post('/favorites', (req, res) => {
+    res.send('Successful Post request added movie to favorites list')
+});
+
+app.delete('/favorites', (req, res) => {
+    res.send('Successful Delete request removed movie to favorites list')
+});
+
+app.delete('/username', (req, res) => {
+    res.send('Successful Delete request removed username')
+});
+
 
 app.listen(8080, () => {
     console.log('My app is listening on port 8080');
