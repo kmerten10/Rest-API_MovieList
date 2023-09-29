8a87703a4607e559e1d17313297841944ba895f2
kmerten10 <kelsey.merten@gmail.com>
2023-09-28 19:59:10 -0500

    Update models.js

--- models.js
+++ models.js
@@ -2,32 +2,32 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
-    Title: {type: String, required: true},
-    Description: {type: String, required: true},
+    Title: { type: String, required: true },
+    Description: { type: String, required: true },
     Genre: [{
-        Name: {type: String},
-        Description: {type: String}    
+        Name: { type: String },
+        Description: { type: String }
     }],
     Director: [{
-        Name: {type: String},
-        Bio: {type: String}  
-}]
+        Name: { type: String },
+        Bio: { type: String }
+    }],
+    Image: { type: String, required: false }
 });
 
 let userSchema = mongoose.Schema({
-    username: {type: String, required: true},
-    password: {type: String, required: true},
-    email: {type: String, required: true},
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
     birth_date: Date,
-    favoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movies'}]
+    favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movies' }]
 });
 
-userSchema.statics.hashPassword = (password) =>
-{
+userSchema.statics.hashPassword = (password) => {
     return bcrypt.hashSync(password, 10);
 };
 
-userSchema.methods.validatePassword = function(password) {
+userSchema.methods.validatePassword = function (password) {
     return bcrypt.compareSync(password, this.password);
 }
 
